62592285e4c088fd0044867af74313796194a107
OMRON Developers Hub <45954736+omron-devhub@users.noreply.github.com>
2021-02-22 20:40:02 +0900

    Update d6f-ph5050.c

--- d6f-ph5050.c
+++ d6f-ph5050.c
@@ -150,26 +150,26 @@ int main() {
     i2c_write_reg16(D6F_ADDR, 0x0B00, NULL, 0);
     delay(900);
 　　while(1){
-		// 2. Trigger getting data (00h, D0h, 40h, 18h, 06h)
-		uint8_t send0[] = {0x40, 0x18, 0x06};
-		i2c_write_reg16(D6F_ADDR, 0x00D0, send0, 3);
+　　　// 2. Trigger getting data (00h, D0h, 40h, 18h, 06h)
+　　　uint8_t send0[] = {0x40, 0x18, 0x06};
+　　　i2c_write_reg16(D6F_ADDR, 0x00D0, send0, 3);
 
-		delay(50);  // wait 50ms
+　　　delay(50);  // wait 50ms
 
-		// 3. Read data (00h, D0h, 51h, 2Ch) (07h)
-		uint8_t send1[] = {0x51, 0x2C};
-		i2c_write_reg16(D6F_ADDR, 0x00D0, send1, 2);
-		uint8_t rbuf[2];
-		uint32_t ret = i2c_read_reg8(D6F_ADDR, 0x07, rbuf, 2);
+　　　// 3. Read data (00h, D0h, 51h, 2Ch) (07h)
+　　　uint8_t send1[] = {0x51, 0x2C};
+　　　i2c_write_reg16(D6F_ADDR, 0x00D0, send1, 2);
+　　　uint8_t rbuf[2];
+　　　uint32_t ret = i2c_read_reg8(D6F_ADDR, 0x07, rbuf, 2);
 
-		uint16_t rd_flow = conv8us_u16_be(rbuf);
-		float flow_rate;
-		
-		// calculation for +/-500[Pa] range
-		flow_rate = ((float)rd_flow - 1024.0) * 1000.0 / 60000.0 - 500.0;
-		
-		printf("%6.2f", flow_rate);
-		printf(" [Pa]\n");
+　　　uint16_t rd_flow = conv8us_u16_be(rbuf);
+　　　float flow_rate;
+	
+　　　// calculation for +/-500[Pa] range
+　　　flow_rate = ((float)rd_flow - 1024.0) * 1000.0 / 60000.0 - 500.0;
+	
+　　　printf("%6.2f", flow_rate);
+　　　printf(" [Pa]\n");
     }
 }
 // vi: ft=c:fdm=marker:et:sw=4:tw=80
